1e25476e9c904750ce99560fef112b342aca9bea
derek.campbell.e <derek.campbell.e@gmail.com>
2018-03-16 23:19:33 -0700

    autocommit

--- bees/workers/cmd.js
+++ bees/workers/cmd.js
@@ -7,8 +7,13 @@ module.exports = function CMDWorker(){
   mind.spawnCmd = function(command, callback){
     let commandArgs = stringArgv(command);
     let spawn = child_process.spawn(commandArgs[0], commandArgs.slice(1));
-    spawn.stderr.on('data', this.error);
-    spawn.stdout.on('data', this.log);
+    let worker = this;
+    spawn.stderr.on('data', function(data){
+      worker.error(data.toString());
+    });
+    spawn.stdout.on('data', function(data){
+      worker.log(data.toString());
+    });
     spawn.on('close', callback);
   };
   return mind;
